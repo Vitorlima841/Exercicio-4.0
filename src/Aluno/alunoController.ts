e70b6f49834efec6f11f89190ad512728b48db7a
Vitorlima841 <vitorauglima@gmail.com>
2024-09-06 14:01:34 -0300

    Projeto finalizado
    Fim do projeto.

--- src/Aluno/alunoController.ts
+++ src/Aluno/alunoController.ts
@@ -9,7 +9,7 @@ export class AlunoController{
 
   @Post()
   async cadastrarAluno(@Body()data: AlunoCadastrarDto): Promise<ResultadoDto>{
-    return this.alunoService.cadastrarAluno(data)//teste
+    return this.alunoService.cadastrarAluno(data)
   }
 
   @Get('ordenados')
